fdbace713b844f5c7899d758925fafdaa12623d8
khoa-d-ho <147029242+khoa-d-ho@users.noreply.github.com>
2025-02-04 21:35:38 -0500

    pcb progress

--- h/pcb.h
+++ h/pcb.h
@@ -12,20 +12,20 @@
 #include "../h/types.h"
 
 extern void freePcb (pcb_t *p);
-extern pcb_t allocPcb ();
+extern pcb_t *allocPcb ();
 extern void initPcbs ();
 
-extern pcb_t mkEmptyProcQ ();
+extern pcb_t *mkEmptyProcQ ();
 extern int emptyProcQ (pcb_t *tp);
 extern void insertProcQ (pcb_t **tp, pcb_t *p);
-extern pcb_t removeProcQ (pcb_t **tp);
-extern pcb_t outProcQ (pcb_t **tp, pcb_t *p);
-extern pcb_t headProcQ (pcb_t *tp);
+extern pcb_t *removeProcQ (pcb_t **tp);
+extern pcb_t *outProcQ (pcb_t **tp, pcb_t *p);
+extern pcb_t *headProcQ (pcb_t *tp);
 
 extern int emptyChild (pcb_t *p);
 extern void insertChild (pcb_t *prnt, pcb_t *p);
-extern pcb_t removeChild (pcb_t *p);
-extern pcb_t outChild (pcb_t *p);
+extern pcb_t *removeChild (pcb_t *p);
+extern pcb_t *outChild (pcb_t *p);
 
 /***************************************************************/
 
--- phase1/pcb.c
+++ phase1/pcb.c
@@ -2,15 +2,85 @@
 #include "../h/types.h"
 #include "../h/pcb.h"
 
+#define MAXPROC	20
+#define	MAXSEM	MAXPROC
+
+static pcb_t pcbFree_list[MAXPROC];
 HIDDEN pcb_t *pcbFree_h;  
 
-/* Process Control Block free list */
+HIDDEN void resetPcb(pcb_t *p) {
+	if (p == NULL)
+    	return;
+    
+    p->p_next = NULL;
+    p->p_prev = NULL;
+    p->p_prnt = NULL;
+    p->p_child = NULL;
+    p->p_sib = NULL;
+    p->p_s.s_entryHI = 0;
+    p->p_s.s_cause = 0;
+    p->p_s.s_status = 0;
+    p->p_s.s_pc = 0;
+    p->p_time = 0;
+    p->p_semAdd = NULL; //
+
+    for (int i = 0; i < STATEREGNUM; i++) {
+        p->p_s.s_reg[i] = 0;
+    }
+}
+
 void freePcb(pcb_t *p) {
     if (p == NULL) {
         return;
     }
+
+    resetPcb(p);
     
-    /* Add to the pcbFree list */
     p->p_next = pcbFree_h;
     pcbFree_h = p;
+}
+
+pcb_t *allocPcb() {
+	if (pcbFree_h == NULL)
+    	return NULL;
+	pcb_t *p = removeProcQ(&pcbFree_h); //
+    
+	resetPcb(p);
+    
+	return p;
+}
+
+void initPcbs() {
+	pcbFree_h = &(pcbFree_list[0]); //
+	for (int i = 0; i < MAXPROC; i++) {
+    	resetPcb(&pcbFree_list[i]);
+    	insertProcQ(&pcbFree_h, &pcbFree_list[i]);
+	}
+}
+
+pcb_t *mkEmptyProcQ() {
+	return NULL;
+}
+
+int emptyProcQ (pcb_t *tp) {
+    return (tp == NULL);
+}
+
+void insertProcQ (pcb_t **tp, pcb_t *p) {
+    if (*tp == NULL) {
+        *tp = p;
+        p->p_next = *tp;
+        p->p_prev = *tp;
+    }
+    else {
+        p->p_next = (*tp)->p_next;
+        p->p_prev = *tp;
+        (*tp)->p_next->p_prev = p;
+        (*tp)->p_next = p;
+        *tp = p;
+    }
+}
+
+pcb_t *removeProcQ (pcb_t **tp) {
+    
 }
